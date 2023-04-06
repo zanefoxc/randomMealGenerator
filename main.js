1b7a639aa2ca76d9a7f7636328e5148ac17daa52
zanefoxc <122697068+zanefoxc@users.noreply.github.com>
2023-04-06 15:54:47 -0400

    simplify data structure, IngredientIdAndParser

--- main.js
+++ main.js
@@ -1,34 +1,33 @@
 // Create an array to hold ingredient option objects
 const ingredientOptions = [
     // Create an object with assigned id and value which will be used later
-    {id: 0, value: 0, proteinOptions: [
-        // Each object has an array of values
-        { _meat: [
+    // Id 0 Protein Options
+    {id: 0, value: 0, options: [
+        // Meat
         "Beef", "Pork", "Lamb",
-        "Venison", "Rabbit"
-    ]}, { _poultry: [
+        "Venison", "Rabbit",
+        // Poultry
         "Chicken", "Turkey", "Duck",
-        "Quail", "Cornish hen", "Goose"
-    ]}, { _seafood: [
+        "Quail", "Cornish hen", "Goose",
+        // Seafood
         "Salmon", "Tuna", "Cod",
         "Trout", "Halibut", "Sardines",
         "Anchovies", "Mackerel", "Shrimp",
         "Crab", "Lobster", "Scallops",
-        "Clams", "Mussels", "Oysters"
-    ]}, { _tofu: [
+        "Clams", "Mussels", "Oysters",
+        // Tofu
         "Silken tofu", "Firm tofu", "Extra-firm tofu",
-        "Super-firm tofu"
-    ]}, { _beans: [
+        "Super-firm tofu",
+        // Beans
         "Black beans", "Kidney beans", "Garbanzo beans (chickpeas)",
         "Navy beans", "Pinto beans", "Cannellini beans",
         "Lima beans", "Great Northern beans", "Adzuki beans",
         "Black-eyed peas", "Butter beans", "Fava beans",
         "Mung beans", "Red beans", "White beans",
         "Soybeans", "Lentils"
-    ]}
     ]},
-    // No additional specificity is needed for vegetables, place them all in the same array
-    {id: 1, value: 0, vegetableOptions: [
+    // Id 1 Vegetable Options
+    {id: 1, value: 0, options: [
         "Broccoli", "Carrots", "Tomatoes",
         "Spinach", "Cabbage", "Bell peppers",
         "Cauliflower", "Eggplant", "Zucchini",
@@ -37,7 +36,8 @@ const ingredientOptions = [
         "Sweet potatoes", "Beets", "Radishes",
         "Brussels sprouts", "Artichokes"
     ]},
-    {id: 2, value: 0, fruitOptions: [
+    // Id 2 Fruit Options
+    {id: 2, value: 0, options: [
         "Apples", "Bananas",  "Oranges",
         "Grapes", "Pineapple", "Strawberries",
         "Blueberries", "Mangoes", "Watermelon",
@@ -47,69 +47,73 @@ const ingredientOptions = [
         "Blackberries", "Raspberries"
     ]},
     // Create an object for dairy the same way as protein above
-    {id: 3, value: 0, dairyOptions: [
-        { _milk: [
+    // Id 3 Diary Options
+    {id: 3, value: 0, options: [
+        // Milk
         "Whole milk", "Skim milk", "Low-fat milk",
         "Fat-free milk", "Almond milk", "Soy milk",
         "Coconut milk", "Oat milk", "Cashew milk",
-        "Rice milk"
-    ]}, { _cream: [
+        "Rice milk",
+        // Cream
         "Heavy cream", "Whipping cream", "Half and half",
         "Light cream", "Coffee creamer", "Sour cream",
-        "Creme fraiche"
-    ]}, { _cheese: [
+        "Creme fraiche",
+        // Cheese
         "Cheddar", "Mozzarella", "Parmesan",
         "Gouda", "Brie", "Camembert",
         "Feta", "Blue cheese", "Gorgonzola",
         "Roquefort", "Emmental", "Gruyere",
         "Colby", "Swiss", "Provolone",
-        "Monterey Jack"
-    ]}, { _yogurt: [
+        "Monterey Jack",
+        // Yogurt
         "Greek yogurt", "Plain yogurt", "Natural yogurt",
         "Balkan yogurt", "Icelandic yogurt", "Skyr",
-        "Labneh", "Kefir"
-    ]}, { _butter: [
+        "Labneh", "Kefir",
+        // Butter
         "Unsalted butter",  "Salted butter", "Whipped butter",
         "Clarified butter", "Cultured butter", "Ghee"
-    ]}
     ]},
-    {id: 4, value: 0, oilOptions: [
+    // Id 4 Oil Options
+    {id: 4, value: 0, options: [
         "Olive oil", "Canola oil", "Vegetable oil",
         "Coconut oil", "Sesame oil", "Peanut oil", 
         "Avocado oil", "Sunflower oil"
     ]},
-    {id: 5, value: 0, spiceOptions: [
-        "allspice", "anise seed", "caraway", "cardamom", "cayenne pepper",
-        "celery seed", "chervil", "chili powder", "cinnamon", "cloves",
-        "coriander", "cumin", "curry powder", "dill seed", "fennel",
-        "fenugreek", "garam masala", "garlic powder", "ginger", "mustard seed",
-        "nutmeg", "onion powder", "oregano", "paprika", "paprika (smoked)",
-        "parsley", "poppy seed", "red pepper flakes", "rosemary", "saffron",
-        "sage", "salt", "savory", "star anise", "sumac",
-        "tarragon", "thyme", "turmeric", "vanilla bean", "wasabi",
-        "white pepper", "black pepper", "cocoa powder"
+    // Id 5 Spice Options
+    {id: 5, value: 0, options: [
+        "Allspice", "Anise Seed", "Caraway", "Cardamom", "Cocoa Powder",
+        "Cayenne Pepper", "Celery Seed", "Chervil", "Chili Powder", 
+        "Cinnamon", "Cloves", "Coriander", "Cumin", "Curry Powder", 
+        "Dill Seed", "Fennel", "Fenugreek", "Garam Masala", "Garlic Powder", 
+        "Ginger", "Mustard Seed", "Nutmeg", "Onion Powder", "Oregano", 
+        "Paprika", "Paprika (Smoked)", "Parsley", "Poppy Seed", "Red Pepper Flakes", 
+        "Rosemary", "Saffron", "Sage", "Salt", "Savory", "Sumac", 
+        "Tarragon", "Thyme", "Turmeric", "Vanilla Bean", "Star Anise", 
+        "Wasabi",  "White Pepper",  "Black Pepper",
+
     ]},
     // Once again organized as protein and dairy
-    {id: 6, value: 0, miscOptions: [
-        { _bread: [
+    // Id 6 Misc Options
+    {id: 6, value: 0, options: [
+        // Bread
         "Baguette", "Brioche", "Challah",
         "Ciabatta", "Focaccia", "Multigrain",
         "Pita", "Rye", "Sourdough", 
-        "White", "Whole wheat"
-    ]}, { _pasta: [
+        "White", "Whole wheat",
+        // Pasta
         "Spaghetti", "Fettuccine", "Linguine",
         "Penne", "Rigatoni", "Farfalle",
         "Macaroni", "Rotini", "Angel hair",
         "Lasagna", "Cannelloni", "Ravioli",
         "Tortellini", "Orzo", "Couscous",
         "Udon", "Soba", "Ramen",
-        "Spätzle"
-    ]}, { _grain: [
+        "Spätzle",
+        // Grain
         "Wheat", "Rice", "Barley",
         "Oats", "Quinoa", "Buckwheat",
         "Cornmeal", "Couscous", "Farro",
-        "Millet", "Polenta"
-    ]}, { _processed: [
+        "Millet", "Polenta",
+        // Processed
         "Baking powder", "Baking soda", "Brown sugar",
         "Cornmeal", "Cornstarch", "Flour",
         "Sugar", "Garlic powder","Onion powder",
@@ -118,10 +122,9 @@ const ingredientOptions = [
         "Ketchup", "Soy sauce", "Worcestershire sauce",
         "Vinegar", "Peanut Butter"
     ]}
-    ]}
 ]
 
-const createComposition = (flavor) => {
+const createCompositionId = (flavor) => {
     const arrMealId = [];
     switch (flavor) {
         case 'Sweet':
@@ -153,13 +156,26 @@ const createComposition = (flavor) => {
         if (ingredientOptions[randomIngredientsId].value > 0) {
             arrMealId.push(randomIngredientsId);
             ingredientOptions[randomIngredientsId].value -= 1;
-            console.log(`Current value of id ${randomIngredientsId} is ${ingredientOptions[randomIngredientsId].value}`);
         } else {
             i--
-        }
-        
+        } 
    }
-   console.log(`Your meal id is ${arrMealId.join('')}`);
+   console.log(arrMealId);
+   return arrMealId;
 }
 
-createComposition('Savory');
+const ingredientIdAndParser = (mealId) => {
+    const arrIngredientId = []
+    const arrIngredients = mealId.map(num => {
+        const ranOptionIndex = Math.floor(Math.random() * ingredientOptions[num].options.length);
+        arrIngredientId.push(ranOptionIndex);
+        return ingredientOptions[num].options[ranOptionIndex];
+    })
+    return {
+        arrIngredients,
+        arrIngredientId
+    };
+}
+
+//console.log(createCompositionId('Savory'));
+console.log(ingredientIdAndParser(createCompositionId('Savory')));
